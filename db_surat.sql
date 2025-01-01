b4557bf62fcebdc9356a962322a78d6d995e1810
alfaturachman <122202202929@mhs.dinus.ac.id>
2025-01-01 21:27:30 +0700

    initial commit

--- db_surat.sql
+++ db_surat.sql
@@ -79,6 +79,48 @@ insert  into `tb_bagian`(`id_bagian`,`nama_bagian`,`username_admin_bagian`,`pass
 (46,'Subkoor MANRU','subkoormanru','4e493962a2203e0712b52cc978495f73e4c5cd15','subkoormanru','2024-12-12','Pusdataru','0247608201','subkoormanru.png'),
 (47,'Subkoor DALTARU','subkoordaltaru','c2b66e94604abaff80bf5189f47be69e407995bf','subkoordaltaru','2024-12-12','Pusdataru','0247608201','subkoordaltaru.png');
 
+/*Table structure for table `tb_surat` */
+
+DROP TABLE IF EXISTS `tb_surat`;
+
+CREATE TABLE `tb_surat` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `kode_surat` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `nomor_surat` varchar(100) DEFAULT NULL,
+  `nomor_urut` varchar(100) DEFAULT NULL,
+  `tanggal` datetime DEFAULT CURRENT_TIMESTAMP,
+  `tanggal_surat` date DEFAULT NULL,
+  `penerima` varchar(255) DEFAULT NULL,
+  `pengirim` varchar(255) DEFAULT NULL,
+  `perihal` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `kategori` enum('Surat Masuk','Surat Keluar') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `file_surat` varchar(255) DEFAULT NULL,
+  `id_bagian_pengirim` int DEFAULT NULL,
+  `id_bagian_penerima` int DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `disposisi_1` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `tanggal_disposisi_1` datetime DEFAULT NULL,
+  `disposisi_2` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `tanggal_disposisi_2` datetime DEFAULT NULL,
+  `disposisi_3` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `tanggal_disposisi_3` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_bagian_pengirim` (`id_bagian_pengirim`),
+  KEY `id_bagian_penerima` (`id_bagian_penerima`),
+  CONSTRAINT `tb_surat_ibfk_1` FOREIGN KEY (`id_bagian_pengirim`) REFERENCES `tb_bagian` (`id_bagian`) ON DELETE SET NULL,
+  CONSTRAINT `tb_surat_ibfk_2` FOREIGN KEY (`id_bagian_penerima`) REFERENCES `tb_bagian` (`id_bagian`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `tb_surat` */
+
+insert  into `tb_surat`(`id`,`kode_surat`,`nomor_surat`,`nomor_urut`,`tanggal`,`tanggal_surat`,`penerima`,`pengirim`,`perihal`,`kategori`,`file_surat`,`id_bagian_pengirim`,`id_bagian_penerima`,`created_at`,`updated_at`,`disposisi_1`,`tanggal_disposisi_1`,`disposisi_2`,`tanggal_disposisi_2`,`disposisi_3`,`tanggal_disposisi_3`) values 
+(3,'4534','3424','1445','2025-01-03 00:00:00','2025-01-01','Ka. Bid PPT','Ka. Bid SBP','Perihal Surat Penting','Surat Keluar','2025-SRT-001.pdf',27,32,'2025-01-01 15:40:27','2025-01-01 20:24:20','Disposisi: Segera diproses','2025-01-02 00:00:00','Disposisi: Untuk perhatian bagian keuangan','2025-01-03 00:00:00','Disposisi: Tindak lanjut oleh bagian hukum','2025-01-04 00:00:00'),
+(5,'4534','3424','1447','2025-01-01 18:15:00','2025-01-01','Ka. Bid SBP','Ka. Bid SBP','dsfadfs','Surat Masuk','2025-3424.pdf',28,32,'2025-01-01 18:15:56','2025-01-01 20:22:30','Kasubag Umpeg','2025-01-01 00:00:00','Subkoor HSI','2025-01-01 00:00:00','Ka. Balai PSDA','2025-01-01 00:00:00'),
+(6,'4534','3424','1448','2025-01-01 18:44:00','2025-01-01','Ka. Bid SBP','Ka. Bid SBP','dfdsag','Surat Masuk','2025-3424.pdf',28,32,'2025-01-01 18:46:24','2025-01-01 20:20:59','',NULL,'',NULL,'',NULL),
+(7,'4534','3424','1449','2025-01-01 18:51:00','2025-01-01','Ka. Balai PSDA','Ka. Bid SBP','dsfasdf','Surat Masuk','2025-3424.pdf',28,32,'2025-01-01 18:51:39','2025-01-01 20:57:20','',NULL,'',NULL,'',NULL),
+(8,'4534','3424','1450','2025-01-01 20:28:00','2025-01-01','Ka. Balai PSDA','Ka. Bid PPT','asassasassasas','Surat Keluar','2025-3424.pdf',28,32,'2025-01-01 20:29:02','2025-01-01 20:29:02','',NULL,'',NULL,'',NULL);
+
 /*Table structure for table `tb_suratkeluar` */
 
 DROP TABLE IF EXISTS `tb_suratkeluar`;
@@ -100,23 +142,18 @@ CREATE TABLE `tb_suratkeluar` (
   UNIQUE KEY `nomor_suratkeluar` (`nomor_suratkeluar`),
   KEY `fk_id_bagian` (`id_bagian`),
   CONSTRAINT `fk_id_bagian` FOREIGN KEY (`id_bagian`) REFERENCES `tb_bagian` (`id_bagian`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=105 DEFAULT CHARSET=latin1;
 
 /*Data for the table `tb_suratkeluar` */
 
 insert  into `tb_suratkeluar`(`id_suratkeluar`,`id_bagian`,`tanggalkeluar_suratkeluar`,`kode_suratkeluar`,`nomor_suratkeluar`,`nama_bagian`,`tanggalsurat_suratkeluar`,`kepada_suratkeluar`,`perihal_suratkeluar`,`file_suratkeluar`,`operator`,`tanggal_entry`) values 
-(27,NULL,'2017-11-15 11:15:00','411','3451/WALIKOTA/2017','WALIKOTA','2017-11-15','Masyarakat','Himbauan Gotong Royong','2017-3451.pdf','admin','2017-11-18 01:25:31'),
-(29,NULL,'2017-11-15 08:20:00','851','3453/TU/2017','TU','2017-11-15','Kepala Bagian Tata Usaha','Cuti Tahunan ','2017-3453.pdf','admin','2017-11-18 02:39:32'),
-(30,NULL,'2017-11-14 13:25:00','915.1','3454/ADM.PEMB/2017','ADM.PEMB','2017-11-15','Walikota','Daftar Usulan Proyek','2017-3454.pdf','admin','2017-11-14 23:29:41'),
-(31,NULL,'2017-11-17 08:30:00','125.4','3455/PEM-OTDA/2017','PEM-OTDA','2017-11-16','Camat Samarida Seberang','Pemekaran Wilayah','2017-3455.pdf','admin','2017-11-16 02:30:02'),
-(35,NULL,'2017-11-17 08:30:00','821.1','3452/TU/2017','TU','2017-11-16','Kepala Bagian Lingkup Pemkot Samarinda','Pengangkatan Jabatan Pegawai Negeri ','2017-3452.pdf','admin','2017-11-16 17:35:35'),
-(88,NULL,'2017-11-17 08:45:00','476.4','3456/KESRA/2017','KESRA','2017-11-17','Lurah SE-KOTA SAMARINDA','Peninjauan Kampung KB','2017-3456.pdf','admin','2017-11-17 02:58:51'),
-(90,NULL,'2017-11-18 08:30:00','376','3458/ASSIII/2017','ASS.III','2017-11-18','Kontraktor Bangunan','Penindakan Bangunan tanpa surat izin mendirikan bangunan','2017-3458.pdf','admin','2017-11-18 03:19:54'),
-(91,NULL,'2017-11-30 01:00:00','454','3457/ORTAL/2017','ORTAL','2017-11-30','Lurah SE-KOTA SAMARINDA','Pelatihan Kelembagaan Desa','2017-3457.pdf','admin','2017-11-30 00:01:06'),
-(92,NULL,'2017-12-06 08:17:00','342','3459/TU/2017','TU','2017-12-06','CAMAT SE-KOTA SAMARINDA','pilgub','2017-3459.pdf','admin','2017-12-06 07:19:29'),
 (96,32,'2024-12-29 06:13:00','476.4','3463','Ka. Bid PPT','2024-12-29','Ka. Balai PSDA','halooo lulll','2024-3463.pdf','Ka. Bid PPT','2024-12-29 06:13:27'),
 (99,28,'2024-12-29 06:43:00','476.4','3466','Ka. Balai PSDA','2024-12-29','Ka. Bid PPT',':(','2024-3466.pdf','Ka. Balai PSDA','2024-12-29 06:43:54'),
-(100,32,'2024-12-29 06:45:00','476.4','3467','Ka. Bid PPT','2024-12-29','Ka. Balai PSDA','lullllll','2024-3467.pdf','Ka. Bid PPT','2024-12-29 06:45:19');
+(100,32,'2024-12-29 06:45:00','476.4','3467','Ka. Bid PPT','2024-12-29','Ka. Balai PSDA','lullllll','2024-3467.pdf','Ka. Bid PPT','2024-12-29 06:45:19'),
+(101,32,'2024-12-29 07:47:00','476.4','3468','Ka. Bid PPT','2024-12-29','Ka. Balai PSDA','fgf','2024-3468.pdf','admin','2024-12-29 07:47:50'),
+(102,28,'2024-12-29 07:54:00','476.4','3469','Kasubag Program','2024-12-29','Ka. Bid PPT','54354','2024-3469.pdf','admin','2024-12-29 07:54:41'),
+(103,32,'2025-01-01 14:46:00','476.4','3470','Ka. Bid PPT','2025-01-01','Ka. Balai PSDA','rrrrrrrrr','2025-3470.pdf','Ka. Bid PPT','2025-01-01 14:46:19'),
+(104,28,'2025-01-01 17:17:00','476.4','3471','Ka. Balai PSDA','2025-01-01','Ka. Bid PPT','ssssssss','2025-3471.pdf','Ka. Balai PSDA','2025-01-01 17:18:21');
 
 /*Table structure for table `tb_suratmasuk` */
 
@@ -135,15 +172,15 @@ CREATE TABLE `tb_suratmasuk` (
   `file_suratmasuk` varchar(255) NOT NULL,
   `operator` varchar(50) NOT NULL,
   `tanggal_entry` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `disposisi1` varchar(50) NOT NULL,
-  `tanggal_disposisi1` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `disposisi2` varchar(50) NOT NULL,
-  `tanggal_disposisi2` varchar(25) NOT NULL,
-  `disposisi3` varchar(50) NOT NULL,
-  `tanggal_disposisi3` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `disposisi1` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `tanggal_disposisi1` datetime DEFAULT CURRENT_TIMESTAMP,
+  `disposisi2` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `tanggal_disposisi2` varchar(25) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `disposisi3` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `tanggal_disposisi3` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_suratmasuk`),
   UNIQUE KEY `nomorurut_suratmasuk` (`nomorurut_suratmasuk`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 /*Data for the table `tb_suratmasuk` */
 
@@ -153,7 +190,9 @@ insert  into `tb_suratmasuk`(`id_suratmasuk`,`tanggalmasuk_suratmasuk`,`kode_sur
 (5,'2017-09-21 15:10:00','660','4520','660.2/1539/100.14','2017-09-19','DINAS LINGKUNGAN HIDUP KOTA SAMARINDA','Sekretaris Daerah','Penting','2017-4520.pdf','admin2','2017-11-14 23:58:01','SEKDA','2017-09-21 23:00:00','PLT.ASS.II','2017-09-24 21:00:00','EKONOMI & SDA','2017-09-25 09:00:00'),
 (6,'2017-09-26 10:00:00','061','4521','061/4382/SJ','2017-09-20','MENDAGRI RI','Organisasi','Surat Edaran Tentang Mekanisme Layanan Administrasi Kemendagri\r\n','2017-4521.pdf','admin','2017-12-02 21:44:11','ASS.III','2017-09-26 15:00:00','','1970-01-01 07:00:00','ORTAL','2017-09-27 11:30:00'),
 (7,'2017-09-25 14:00:00','503','4522','503/744/100.26','2017-09-25','DINAS PENANAMAN MODAL DAN PELAYANAN TERPADU SATU PINTU KOTA SAMARINDA','PLH SEKDA','Tindak Lanjut Permohonan Penghapusan Denda Retribusi IMB PT.Borneo Inti Graha\r\n','2017-4522.pdf','admin','2017-12-06 00:32:23','PLH.SEKDA','2017-09-26 10:00:00','','1970-01-01 07:00:00','HUKUM','2017-09-27 15:00:00'),
-(8,'2017-12-06 08:12:00','454','4523 ','4121/wawali/2017','2017-12-06','pdam','wawali','air','2017-4523.pdf','admin','2017-12-06 07:15:07','WAKIL WALIKOTA','2017-12-14 08:14:00','ADM.PEMB','2017-12-12 08:14:00','PEM-OTDA','2017-12-13 08:15:00');
+(8,'2017-12-06 08:12:00','454','4523 ','4121/wawali/2017','2017-12-06','pdam','wawali','air','2017-4523.pdf','admin','2017-12-06 07:15:07','WAKIL WALIKOTA','2017-12-14 08:14:00','ADM.PEMB','2017-12-12 08:14:00','PEM-OTDA','2017-12-13 08:15:00'),
+(10,'2025-01-01 09:00:00','66','4524 ','56','2025-01-01','Ka. Bid PPT','Ka. Bid TARU','sdfasdf','2025-4524.pdf','Ka. Bid PPT','2025-01-01 09:00:54','Kasubag Umpeg','2025-01-01 09:00:00','Kasubag Umpeg','2025-01-01 09:00:00','Ka. Bid IAB','2025-01-08 09:01:00'),
+(11,'2025-01-01 15:06:00','12334','4525 ','34','2025-01-01','Ka. Balai PSDA','Ka. Bid PPT','pppppppppppppppppp','2025-4525.pdf','Ka. Balai PSDA','2025-01-01 15:07:15','','1970-01-01 07:00:00','','1970-01-01 07:00:00','','1970-01-01 07:00:00');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
