58ceb6e0def610622a19d67cf725ed9e7416b9cd
alfaturachman <122202202929@mhs.dinus.ac.id>
2025-01-05 20:42:09 +0700

    initial commit

--- bagian/inputsuratmasuk.php
+++ bagian/inputsuratmasuk.php
@@ -75,7 +75,7 @@ include "login/ceksession.php";
                 </div>
                 <div class="x_content">
                   <br />
-                  <form action="proses/proses_inputsuratmasu.php" name="formsuratmasuk" method="post" enctype="multipart/form-data" id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
+                  <form action="proses/proses_inputsuratmasuk.php" name="formsuratmasuk" method="post" enctype="multipart/form-data" id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
                     <div class="form-group">
                       <label class="control-label col-md-3 col-sm-3 col-xs-12" for="kategori">Kategori <span class="required">*</span></label>
                       <div class="col-md-9 col-sm-9 col-xs-12">
